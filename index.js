a2b0939d5ffa8aba8653fafa4d7647ea0f20988a
sukhdeep3 <sukhdeepsukh589@gmail.com>
2022-11-23 03:10:39 -0500

    Set controllers, routes and ejs

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const port = 8000;
 
 const app = express();
 
+app.use("/", require("./routes"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.listen(port, function (err) {
   if (err) {
     console.log("Error in loading server", err);
+    return;
   }
   console.log("Server is running on port:", port);
 });
